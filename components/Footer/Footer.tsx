839f21cb380d5e7db6da6997d39de6ab6aa6b84c
Savva Dzhumaliev <pro100prosavva@gmail.com>
2020-05-10 19:46:54 +0400

    initial footer implementation

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,5 +3,14 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 export function Footer() {
-  return <footer className={styles.footer}>footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <p className={styles.copyright}>
+          &copy; 2020
+          <span className={styles.logo}> Р-строй</span>
+        </p>
+      </div>
+    </footer>
+  );
 }
